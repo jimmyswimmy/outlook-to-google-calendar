b86ac97220699285756541dc8141af104b1127c0
Friel, Jim <jfriel@arete.com>
2024-09-05 17:52:21 -0400

    Add new sample config with tz_offset and paths

--- config_sample.py
+++ config_sample.py
@@ -1,17 +1,14 @@
 # outlook
-outlook_client_id = "your-client-id-here"
-outlook_client_secret = "your-client-secret-here"
-outlook_scopes = ["basic", "calendar"]
-outlook_token_path = "./credentials/"
-outlook_token_filename = "outlook_token.txt"
-previous_days = 40  # retrieve this many past days of events
-future_days = 365  # retrieve this many future days of events
+previous_days = 3  # retrieve this many past days of events
+future_days = 7  # retrieve this many future days of events
 
 # google
-google_token_path = "./credentials/google_token.pickle"
-google_calendar_id = "your-calendar-id-here@group.calendar.google.com"
+google_creds_path = "./credentials/credentials.json"
+google_token_path = "./credentials/google_token.json"
+google_calendar_id = "primary" #think this is the default otherwise change it to the calendar you want to write
 
 # misc
 events_ts_json_path = "./events_ts.json"
+system_tz_offset = 4
 pause = 0.1
 force = False  # force full run even if no changes
